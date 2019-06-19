1c42b5a75867cdcc9189d1efa3e1c1b024b9692d
MrunalShidurkar <mrunal.shidurkar@gmail.com>
2019-06-19 17:10:57 +0530

    width added

--- Components/Main.js
+++ Components/Main.js
@@ -3,7 +3,7 @@ import Flex from "../lib";
 const Main = () => {
   return (<Flex flexIf={true} direction={{xs:"column"}} justify={{xs:"center"}} align={{xs:"center"}}>
     <Flex show={{xs:true}}> I'm vertically and horizontally centered!1</Flex>
-      <Flex hide={{xs:true}}>I'm vertically and horizontally centered!2</Flex>
+      <Flex  width={{xs:"50%"}}>I'm vertically and horizontally centered!2</Flex>
   </Flex>
   )
 };
